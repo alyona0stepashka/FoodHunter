db00e104e01dd684385cac10483c0c0f592455a2
Alena Pakholko <alena.pakholko@oxagile.com>
2020-02-04 14:06:28 +0300

    added interfaces

---
+++ FH.App/FH.DAL/EF/Interfaces/IFullRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using FH.Models;
+
+namespace FH.DAL.EF.Interfaces
+{
+    interface IFullRepository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        List<TEntity> GetWhere(Expression<Func<TEntity, bool>> predicate);
+    }
+}
---
+++ FH.App/FH.DAL/EF/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FH.Models;
+
+namespace FH.DAL.EF.Interfaces
+{
+    interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<TEntity> CreateAsync(TEntity item);
+        TEntity Update(TEntity item);
+        Task<TEntity> DeleteAsync(int id);
+        List<TEntity> GetAll();
+        Task<TEntity> GetByIdAsync(int id);
+        Task Save();
+    }
+}
