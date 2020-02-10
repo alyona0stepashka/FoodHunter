b37a203f405b50407830c533c5d7eebbe0ad5007
Alena Pakholko <alena.pakholko@oxagile.com>
2020-02-10 13:42:34 +0300

    added guards

---
+++ FH.UI/angular/src/app/guards/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, CanActivate } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+
+  constructor(private router: Router) {
+  }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    if (localStorage.getItem('token') != null){ 
+      return true;
+    }
+    else {
+      this.router.navigate(['/auth/login']);
+      return false;
+    }
+
+  }
+}
