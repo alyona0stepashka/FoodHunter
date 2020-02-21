bc1c37ead21d9f2adefd066b740e13efe4a1d794
Alena Pakholko <alena.pakholko@oxagile.com>
2020-02-21 14:34:39 +0300

    added default

--- FH.UI/angular/src/app/pages/menu-manager/menu-manager/menu-manager.component.ts
+++ FH.UI/angular/src/app/pages/menu-manager/menu-manager/menu-manager.component.ts
@@ -3,10 +3,8 @@ import { MenuService } from 'app/services/menu.service';
 import { StaticService } from 'app/services/static.service';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { environment } from 'environments/environment';
 import { ActivatedRoute } from '@angular/router';
-import { Lightbox } from 'ngx-lightbox';
 
 @Component({
   selector: 'app-menu-manager',
@@ -15,29 +13,40 @@ import { Lightbox } from 'ngx-lightbox';
 })
 export class MenuManagerComponent implements OnInit {
 
-  public isNotFound = false;
-
   constructor(
     private menuService: MenuService,
     private staticService: StaticService,
-    private lbLightbox: Lightbox,
     private formBuilder: FormBuilder,
     private toastr: ToastrService,
-    private activateRoute: ActivatedRoute,
-    private modalService: NgbModal) { }
+    private activateRoute: ActivatedRoute) { }
 
-  closeResult: string;
+  //logic vars
+  currentMenuId: number;
+  serverUrl = environment.serverURL;
+  //logic vars
+
+  //firat tab form
   menuForm: FormGroup;
-  menuItemForm: FormGroup;
+  submittedMenu = false;
+  //firat tab form
+
+  //my locationId & locationId from url
+  locationId = localStorage.getItem("MyLocationId");
+  welcomeLocationId;
+  isLocationExist = (this.locationId != null && this.locationId != '0');
+  //myLocationId & locationId from url
+
+  //bool vars for role access
   public isLogin = (localStorage.getItem('token') != null);
-  public locationId = localStorage.getItem("MyLocationId");
-  public welcomeLocationId;
-  public isLocationExist = (this.locationId != null && this.locationId != '0');
   public isManager = ((this.isLogin) && (localStorage.getItem('IsManager').toLocaleLowerCase() == 'true'));
   public isCurrentUser = ((this.isLogin) && (localStorage.getItem('CurrentRole').toLocaleLowerCase() == 'false'));
   public isEdit = (this.isLocationExist && this.isManager && !this.isCurrentUser);
+  //bool vars for role access
+
+  //server lists
   public menus = new Array();
   public icons = new Array();
+  //server lists
 
   async ngOnInit() {
     await this.activateRoute.params.subscribe(params => this.welcomeLocationId = params.id);
@@ -65,19 +74,84 @@ export class MenuManagerComponent implements OnInit {
       Info: ['', [Validators.required]],
       IconId: ['', [Validators.required]]
     });
-    this.menuItemForm = this.formBuilder.group({
-      Id: [''],
-      Title: ['', [Validators.required]],
-      Info: ['', [Validators.required]],
-      Price: ['', [Validators.required]],
-      PriceWithSales: [''],
-      IsActive: [this.IsActive, [Validators.required]],
-      MenuId: ['', [Validators.required]],
-      Note: ['', [Validators.required]],
-      Photo: ['', [Validators.required]],
-      // IconId: ['']
-    });
     this.loadLocationMenu();
+  }
+
+  loadLocationMenu() {
+    this.menuService.getLocationMenus(this.welcomeLocationId).subscribe(
+      res => {
+        this.menus = res as [];
+        this.staticService.getIcons().subscribe(
+          res => {
+            this.icons = res as [];
+          },
+          err => {
+            console.log(err);
+            this.toastr.error(err.error, 'Error');
+          }
+        );
+      },
+      err => {
+        console.log(err);
+        this.toastr.error(err.error, 'Error');
+      }
+    );
+  }
+
+  get f() { return this.menuForm.controls; }
+
+  onSubmitMenu() {
+    this.submittedMenu = true;
+    if (this.menuForm.invalid) {
+      return null;
+    }
+    this.menuService.createMenu(this.menuForm, this.welcomeLocationId).subscribe(
+      (res: any) => {
+        this.toastr.success(
+          '<span data-notify="icon" class="nc-icon nc-bell-55"></span><span data-notify="message">Your business location is registered</span>',
+          "",
+          {
+            timeOut: 4000,
+            closeButton: true,
+            enableHtml: true,
+            toastClass: "alert alert-success alert-with-icon"
+          }
+        );
+        this.loadLocationMenu();
+      },
+      err => {
+        console.log(err);
+        this.toastr.error(err.error, 'Error');
+      }
+    );
+  }
+
+  deleteMenu(id) {
+    this.menuService.deleteMenu(id).subscribe(
+      (res: any) => {
+        this.toastr.success(
+          '<span data-notify="icon" class="nc-icon nc-bell-55"></span><span data-notify="message">Your business location is registered</span>',
+          "",
+          {
+            timeOut: 4000,
+            closeButton: true,
+            enableHtml: true,
+            toastClass: "alert alert-success alert-with-icon"
+          }
+        );
+        this.loadLocationMenu();
+      },
+      err => {
+        console.log(err);
+        this.toastr.error(err.error, 'Error');
+      }
+    );
+  }
+
+  setCurrentMenuId(id) {
+    this.currentMenuId = id;
+    console.log(id);
+
   }
 
 }
